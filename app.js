34bae93d6ce377a8bf190b0fd713d211aa0e7e30
PranavBarthwal <pbarthwal90@gmail.com>
2023-12-25 15:50:24 +0530

    added js functionality

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-const API_KEY = "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"
+const API_KEY = "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"
+
+const submitIcon = document.querySelector('#button-addon2')
+
+const inputElement =  document.querySelector('.form-control')
+
+const getImages = async () => {
+    
+    const options = {
+        method : "POST",
+        headers : {
+            "Authorization" : `Bearer ${API_KEY}`,
+            'Content-Type' : "application/json"
+        },
+        body : JSON.stringify({
+            "prompt" : "inputElement.value",
+            "n" : 1,
+            "size" : "1024x1024"
+        })
+    }
+
+    
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+
+        const data = await response.json()
+
+        console.log(data)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+submitIcon.addEventListener('click', getImages())
